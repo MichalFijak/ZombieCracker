0c3930453f193be5c55488132985587b215c81dd
MichalFijak <michalfijak1@hotmail.com>
2021-11-16 16:42:26 +0100

    Base ZombieCracker
    UNITYGAME ZC

---
+++ Enemy.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    public float health = 50f;
+
+
+    // Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attack
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if (!playerInAttackRange && !playerInSightRange) Patroling();
+        if (!playerInAttackRange && playerInSightRange) Chasing();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+
+    }
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+    }
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) 
+            walkPointSet = true ;
+    }
+    private void Chasing()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if(!alreadyAttacked)
+        {
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if(health<=0)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
---
+++ GunScript.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public float impact = 30f;
+    public float fireRate = 15f;
+    public AudioSource audioSource;
+
+    private float nextTimeToFire = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1")&&Time.time>=nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+
+            shoot();
+        }
+    }
+    public void shoot()
+    {
+        audioSource.Play();
+        muzzleFlash.Play();
+        RaycastHit hit;
+        
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if(enemy!=null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impact);
+            }
+            GameObject impactGO=Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
